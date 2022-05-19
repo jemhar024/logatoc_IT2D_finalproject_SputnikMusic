ed0dd31af94dd1e9aa0ffe797fc0d00dafa30cf8
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-20 01:55:59 +0800

    Fourteenth Commit

---
+++ information.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="with=device-width, initial-scale=1.0">
+	<title>Sputnik Music</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	 <section class="banner">
+		<div class="navbar">
+           <img src="media/Picture3.png" class="logo">
+            <div class="nav-links" id="navLinks">
+               <ul>
+                   <li><a href="index.php" >Home</a></li>
+                   <li><a href="library.php">Library</a></li>
+                   <li><a href="About.php">About Us</a></li>
+                   <li><a href="contact.php"class="active">Contact</a></li>
+                   <li><a href="blog1.php">Blog</a></li>
+              </ul>
+            </div>   
+            </div> 
+
+		<h1>Contact</h1>
+
+
+<!------------------Information---------------->
+<?php
+       echo "<section> 
+       	<div class='nav'>
+              <ul>
+                   <li><a href='contact.php'>Contact</a></li>
+                   <li><a href='information.php'class='active'>Information</a></li>
+              </ul>
+        </div>
+        </section>
+    </section>";
+
+   echo"  <section class='about_us'>
+    <div class='row'>
+        <div class='about_col'>
+            <h1>Jared Simon R. Danao</h1>
+             <h2>Web-Developer</h2>";
+           echo" <p>H! I am Jared Simon R. Danao. 20 year of age, born on November 24, 2022. I currently live in Sitio Malilay II, Brgy. Matalaba, Santa Cruz, Marinduque.I studied in Marinduque State College in Tanza, Boac Marinduque, 2nd year student. I am taking Bachelor of Science Information Technology. My hobbies are playing basketball, playing online games like Mobile Legend, Call of Duty, Clash of Clan, riding motorcycle, watching anime, and adventure. I also love listening to musics because it makes comfortable and peace.</p>"; 
+              echo"  <p>Experienced web developer with a strong background in developing award-winning applications for a diverse clientele. 5+ years of industry experience includes programming, debugging, and wireframes. Received 80%+ experience scores on every web application built for SoftwareX. </p>
+        </div>";
+        echo" <div class='about_col'>
+            <img src='media/web1.jpg'>
+        </div>
+    </div>";
+
+
+
+    echo" <div class='row'>
+        <div class='about_col'>
+            <h1>Jemhar Jhon Logatoc</h1>
+             <h2>Web-Developer</h2>";
+            echo" <p>I'm jemhar jhon L. Logatoc, 19 years old from mahinhin, boac, marinduque. I studied elementary at mahinhin elementary svchool, then i tried to study in ilaya national high school but it is so hard so i decided to transfer in puting buhangin national high school. When i am in senior high school i take computer system servicing major. Currently I am  taking bachelor of science in information technology at marinduque state college. I used to play basketball and mobiles games on my free time but it mostly consumed in playing basketball. I love to listen to the music so i wondered to create my own website to easily access my favorite songs. You may contact me on my social media account Facebook: Jemhar Logatoc, Gmail:jemharlandoylogatoc@gmail.com, also you can watch me on my YouTube channel but all of the videos out there is for activities only.</p>";
+              echo" <p>'Enhancing my knowledge in programming is my focus, perseverance is my investment, sucessful life is my goal'</p>
+        </div>
+        <div class='about_col'>
+            <img src='media/web2.jpg'>
+        </div>
+    </div>
+    
+</section>";
+?>
+
+     <!----------Footer---------->
+<section class="footer">
+    <h4>About Us</h4>
+   
+     <div class="icons">
+        <i class="fa fa-facebook"></i>
+        <i class="fa fa-twitter"></i>
+        <i class="fa fa-instagram"></i>
+        <i class="fa fa-youtube"></i>
+        <i class="fa fa-pinterest"></i>
+    </div>
+   
+    <p> It is a long established fact that a reader will be distracted by the readable content.<br>&copy; 2022 Sputnik Music. Marinduque-Philippines. All Right Reserved <br> Founded by Jared Simon R. Danao <br> Jemhar Jhon Logatoc</p>
+
+</section>
+
+
+<!-----------------javascript for toggle menu----------------->
+    <script type="text/javascript">
+        
+        var navLinks = document.getElementById("navLinks");
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+
+
+    </script>
+</body>
+</html>
