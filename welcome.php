afa291c1b9d6557a37ea0a97b59475c91638c722
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-20 00:50:44 +0800

    First Commit

---
+++ welcome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Sputnik Music</title>
+</head>
+<?php
+echo" <body>
+    <section class='banner3'>
+          <div class='navbar2'>
+           <img src='media/Picture3.png' class='logo'>    
+             <div class='text-box2'>
+                <h3>WELCOME TO</h3>
+                <h1>Sputnik Music</h1><br>
+                <a href='index.php' class='hero-btn'>Get Started</a> 
+            </div>
+         </div>
+    </section>";
+    ?>
+</body>
+</html>
